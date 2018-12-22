9c3b4f4f66534d3c0e5bd6ddbdbe02f087381fa7
Никита Воробьёв <nikitavorobev@macbook-pro-nikita-2.local>
2018-12-22 16:32:26 +0300

    Initial Commit

---
+++ chess tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chess tutorial
+//
+//  Created by Никита Воробьёв on 22/12/2018.
+//  Copyright © 2018 Никита Воробьёв. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
